77204f595682177b74ef519aa8686a49bd9495c6
Arina <arisha20151@yandex.ru>
2024-11-07 12:31:52 +0300

    First

--- Form1.cs
+++ Form1.cs
@@ -2,9 +2,23 @@ namespace WinFormsApp
 {
     public partial class Form1 : Form
     {
+        Brush? brush;
         public Form1()
         {
             InitializeComponent();
+        }
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            brush = Brushes.Yellow;
+            Graphics graphics = CreateGraphics();
+            graphics.FillEllipse(brush, 0, 0, 10, 20);
+            graphics.Dispose();
+
+            brush = Brushes.Green;
+            graphics = CreateGraphics();
+            graphics.FillEllipse(brush, 20, 50, 100, 100);
+            graphics.Dispose();
         }
     }
 }
